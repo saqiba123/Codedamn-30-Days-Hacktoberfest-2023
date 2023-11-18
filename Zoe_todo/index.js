5700ee6a1ed1e01511a201ebb1950d763388dfa6
saqiba123 <saqibajuna@gmail.com>
2023-11-18 10:08:58 +0500

    codedamn daily practice

---
+++ Zoe_todo/index.js
@@ -0,0 +1,16 @@
+function addTask() {
+	//js practice
+	let taskText = document.getElementById("taskInput").value;
+	if (taskText === "") return;
+
+	let li = document.createElement("li");
+	li.textContent = taskText;
+	li.addEventListener("click", markCompleted);
+
+	document.getElementById("taskList").appendChild(li);
+	document.getElementById("taskInput").value = "";
+}
+
+function markCompleted(event) {
+	event.target.classList.toggle("completed");
+}
